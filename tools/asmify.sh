e3f7894f26a97397ce52b1e59d7ed8753b81917d
vitalyb <vitalyb@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2007-06-16 19:21:18 +0000

    * added script to convert IDAs asm dump to something similar to gcc's inline asm
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@175 6794e30b-3f2a-0410-a806-a2bbca1c07ff

---
+++ tools/asmify.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# script to convert IDAs asm dump to something similar to gcc's inline asm
+
+sed 's/^ROM:[0-9A-F]* \?//' | sed 's/[[:space:]]*;.*$//' | grep -v '^$' | \
+sed 's/^\(loc_[0-9A-F]*\)$/\1:/' | \
+sed 's/^\([[:space:]]*\)DCD/\1.long/' | \
+sed 's/^\([[:space:]]*\)\(.*\)$/\1\"\2\\n\"/'
