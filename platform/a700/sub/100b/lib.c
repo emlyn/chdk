610004b795399e236531f34e3e5a9e7d55e78e12
fudgey <fudgey@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2009-06-09 21:42:35 +0000

    MD speedup for a700 from TonyH in http://chdk.setepontos.com/index.php/topic,405.msg35010.html#msg35010
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@773 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- platform/a700/sub/100b/lib.c
+++ platform/a700/sub/100b/lib.c
@@ -22,7 +22,16 @@ long hook_raw_size()
 
 void *vid_get_viewport_live_fb()
 {
-    return (void*)0;
+//    return (void*)0x0;
+    void **fb=(void **)0x5264;
+    unsigned char buff = *((unsigned char*)0x5274);
+    if (buff == 0) {
+        buff = 2;
+    }
+    else {
+        buff--;
+    }
+    return fb[buff];
 }
 
 void *vid_get_bitmap_fb()
