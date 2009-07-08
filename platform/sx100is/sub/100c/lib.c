2572608cf06c7a70ddeef1425a6a314c1d02a295
fudgey <fudgey@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2009-07-08 20:35:07 +0000

    Motion detector speedup for sx100is 100c from Anaglyphic http://chdk.setepontos.com/index.php/topic,405.msg36159.html#msg36159
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@783 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- platform/sx100is/sub/100c/lib.c
+++ platform/sx100is/sub/100c/lib.c
@@ -37,7 +37,16 @@ long hook_raw_size()
 
 void *vid_get_viewport_live_fb()
 {
-	return (void*)0;//0x10670ee0;
+//	return (void*)0;//0x10670ee0;
+    void **fb=(void **)0x21E4;
+    unsigned char buff = *((unsigned char*)0x2098);
+    if (buff == 0) {
+        buff = 2;
+    }
+    else {
+        buff--;
+    }
+    return fb[buff];
 }
 
 void *vid_get_bitmap_fb()
