683fce645f04e19815944a3b00f82ec04e1d68a9
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2009-07-20 01:22:23 +0000

    Revert unneeded change in boot.c
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@788 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- platform/a630/sub/100c/boot.c
+++ platform/a630/sub/100c/boot.c
@@ -18,15 +18,6 @@ void __attribute__((naked,noinline)) h_usrInit();
 void __attribute__((naked,noinline)) h_usrKernelInit();
 void __attribute__((naked,noinline)) h_usrRoot();
 
-void taskCreateHook(int *p) {
- p-=16;
- if (p[0]==0xFFCFBDF8)  p[0]=(int)exp_drv_task;
-}
-
-void taskCreateHook2(int *p) { 
- p-=16;
- if (p[0]==0xFFCFBDF8)  p[0]=(int)exp_drv_task;
-}
 
 
 void boot()
@@ -59,10 +50,6 @@ void boot()
 	"ORR     R0, R0, #1\n"
 	"MCR     p15, 0, R0,c1,c0\n"
     :::"r0");
-
-   *(int*)0x1930=(int)taskCreateHook;
-   *(int*)0x1934=(int)taskCreateHook2;
-
 
     h_usrInit();
 }
