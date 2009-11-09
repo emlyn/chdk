97fe6b751535e458187e6dbeacda9f243a98bebc
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2009-11-09 00:25:54 +0000

    a590 LED control from ewavr, see http://chdk.setepontos.com/index.php/topic,2361.msg43042.html#msg43042
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@832 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- platform/a590/wrappers.c
+++ platform/a590/wrappers.c
@@ -20,24 +20,16 @@ long lens_get_target_distance()
 
 
 void ubasic_set_led(int led, int state, int bright)
-{ 
-        struct led_control led_c; 
-        char convert_table[11]={0,1,2,3,0,2,3,1,8,10,10};  // s3 to a710 (and a720) convert table
-
-        //char convert_table[6]={0,1,2,3,8,10};  // Test a720, values 0-5 are valid
-
+{
         // 0 gr  
-        // 1 red 
+        // 1 orange 
         // 2 yel 
-        // 3 pw  
         // 8 dp  
         // 9 af  
 
-        led_c.led_num=convert_table[led%11]; 
-        led_c.action=state<=1 ? !state : state; 
-        led_c.brightness=bright; 
-        led_c.blink_count=255; 
-        _PostLEDMessage(&led_c); 
+  int leds[] = {0,2,8,1,8,9};
+  if(led < 4 || led > 9 || led == 6) return;
+  _LEDDrive(leds[led-4], state<=1 ? !state : state);
 } 
 
 
