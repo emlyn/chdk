a08b5e49a205ac6ed1156fa068384376fddd60c8
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2010-05-30 09:29:35 +0000

    add romlog.lua
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@895 6794e30b-3f2a-0410-a806-a2bbca1c07ff

---
+++ CHDK/SCRIPTS/TEST/romlog.lua
@@ -0,0 +1,51 @@
+--[[
+@title save ROM crash log to ROMLOG.LOG
+requires CHDK built with native call support
+@param a remove old files 1=yes, 0=no
+@default a 1
+--]]
+LOG_NAME="A/ROMLOG.LOG"
+ERR_NAME="A/RomLogErr.txt"
+
+function printf(...)
+	print(string.format(...))
+end
+
+if (type(call_event_proc) ~= "function" ) then
+	error("your CHDK does not support native calls")
+end
+
+if (call_event_proc("SystemEventInit") == -1) then
+	printf("SystemEventInit failed")
+end
+
+if a ~= 0 then
+	if (os.stat(LOG_NAME)) then
+		printf("rm %s: %s",LOG_NAME,tostring(os.remove(LOG_NAME)));
+	end
+	if (os.stat(ERR_NAME)) then
+		printf("rm %s: %s",ERR_NAME,tostring(os.remove(ERR_NAME)));
+	end
+end
+-- first arg: filename, NULL for ROMLOG.TXT (dryos) or ROMLOG (vxworks)
+-- second arg: if 0, shutdown camera after writing log
+-- note, on vxworks the exception code, registers and stack trace are binary
+r=call_event_proc("GetLogToFile",LOG_NAME,1) 
+
+if r ~= 0 then
+	printf("GetLogToFile return %d",r)
+end
+if (os.stat(ERR_NAME)) then
+	if a ~= 0 then
+		printf("FAIL check %s",ERR_NAME)
+	else
+		printf("%s exists",ERR_NAME)
+	end
+end
+if (os.stat(LOG_NAME)) then
+	printf("%s exists",LOG_NAME)
+else
+	printf("%s not written",LOG_NAME)
+end
+sleep(2000)
+
