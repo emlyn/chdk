0178cf21bf648570a31b29283ba902c6cbcea9a4
reyalp <reyalp@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2010-08-17 06:10:07 +0000

    update dancingbits for a3100 and possibly other new cameras (untested)
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@921 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- tools/dancingbits.c
+++ tools/dancingbits.c
@@ -12,11 +12,12 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define VITALY 3
+#define VITALY 4
 unsigned char _chr_[VITALY][8] = {
 									{ 4,6,1,0,7,2,5,3 }, // original flavor
 									{ 5,3,6,1,2,7,0,4 }, // nacho cheese sx200is, ixus100_sd780, ixu95_sd1200, a1100, d10
-									{ 2,5,0,4,6,1,3,7 }  // mesquite bbq ixus200_sd980, sx20
+									{ 2,5,0,4,6,1,3,7 }, // mesquite bbq ixus200_sd980, sx20 (dryos r39)
+									{ 4,7,3,2,6,5,0,1 }, // cool ranch a3100 (dryos r43)
 								};
 
 unsigned char dance(unsigned char allbest, int fudgey);
