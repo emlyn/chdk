d8fd9499837a548f97e3ce802331646396d4aea7
fe50 <fe50@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2010-08-22 18:29:09 +0000

    * typo correction camera.h for a450, cleanup ;)
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@926 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- include/camera.h
+++ include/camera.h
@@ -196,42 +196,43 @@
 //----------------------------------------------------------
 
 #elif defined (CAMERA_a450)
-    #define CAM_PROPSET                 2
+	#define CAM_PROPSET						2
 
-    #define CAM_RAW_ROWPIX              2664   // for 5 MP 1/3" sensor size
-    #define CAM_RAW_ROWS                1968   // for 5 MP 1/3" sensor size
+	#define CAM_RAW_ROWPIX					2664   // for 5 MP 1/3" sensor size
+	#define CAM_RAW_ROWS					1968   // for 5 MP 1/3" sensor size
 
-    #undef  CAM_USE_ZOOM_FOR_MF
-    #undef  CAM_HAS_ZOOM_LEVER#define CAM_DRAW_EXPOSITION         1
-    #undef  CAM_HAS_ERASE_BUTTON
-    #undef  CAM_HAS_IRIS_DIAPHRAGM
-    #define CAM_HAS_ND_FILTER           1
-    #undef  CAM_HAS_MANUAL_FOCUS
-    #undef  CAM_HAS_USER_TV_MODES
-    #define CAM_SHOW_OSD_IN_SHOOT_MENU  1
-    #undef  CAM_HAS_IS
-    #define CAM_CAN_MUTE_MICROPHONE     1
-    #define CAM_AF_SCAN_DURING_VIDEO_RECORD 1
-    #define CAM_EV_IN_VIDEO             1
-    #define DNG_SUPPORT                 1
-    // pattern
-    #define cam_CFAPattern 0x02010100 // Red  Green  Green  Blue
-    // color
-    #define CAM_COLORMATRIX1                               \
-      652279,  1000000,-199279, 1000000, -101833, 1000000, \
-     -159819,  1000000, 619308, 1000000,   78172, 1000000, \
-      -58827,  1000000, 150963, 1000000,  264216, 1000000 
+	#undef  CAM_USE_ZOOM_FOR_MF
+	#undef  CAM_HAS_ZOOM_LEVER
+	#define CAM_DRAW_EXPOSITION				1
+	#undef  CAM_HAS_ERASE_BUTTON
+	#undef  CAM_HAS_IRIS_DIAPHRAGM
+	#define CAM_HAS_ND_FILTER				1
+	#undef  CAM_HAS_MANUAL_FOCUS
+	#undef  CAM_HAS_USER_TV_MODES
+	#define CAM_SHOW_OSD_IN_SHOOT_MENU		1
+	#undef  CAM_HAS_IS
+	#define CAM_CAN_MUTE_MICROPHONE			1
+	#define CAM_AF_SCAN_DURING_VIDEO_RECORD	1
+	#define CAM_EV_IN_VIDEO					1
+	#define DNG_SUPPORT						1
+	// pattern
+	#define cam_CFAPattern 0x02010100 // Red  Green  Green  Blue
+	// color
+	#define CAM_COLORMATRIX1                               \
+      652279,  1000000, -199279, 1000000, -101833, 1000000, \
+     -159819,  1000000,  619308, 1000000,   78172, 1000000, \
+      -58827,  1000000,  150963, 1000000,  264216, 1000000
 
-    #define cam_CalibrationIlluminant1 1 // Daylight
-    // cropping
-    #define CAM_JPEG_WIDTH  2592
-    #define CAM_JPEG_HEIGHT 1944
-    #define CAM_ACTIVE_AREA_X1 6
-    #define CAM_ACTIVE_AREA_Y1 6
-    #define CAM_ACTIVE_AREA_X2 2618
-    #define CAM_ACTIVE_AREA_Y2 1962
-    // camera name
-    #define PARAM_CAMERA_NAME 4 // parameter number for GetParameterData
+	#define cam_CalibrationIlluminant1		1 // Daylight
+	// cropping
+	#define CAM_JPEG_WIDTH			2592
+	#define CAM_JPEG_HEIGHT			1944
+	#define CAM_ACTIVE_AREA_X1		6
+	#define CAM_ACTIVE_AREA_Y1		6
+	#define CAM_ACTIVE_AREA_X2		2618
+	#define CAM_ACTIVE_AREA_Y2		1962
+	// camera name
+	#define PARAM_CAMERA_NAME		4 // parameter number for GetParameterData
 //----------------------------------------------------------
 
 #elif defined (CAMERA_a460)
