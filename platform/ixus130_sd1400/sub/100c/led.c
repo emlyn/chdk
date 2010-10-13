bc347f52135b2be435b4670ae7595c6d729f678e
Emlyn <emlyn@corrin.name>
2010-10-13 10:28:11 +0200

    Added LED functions

---
+++ platform/ixus130_sd1400/sub/100c/led.c
@@ -0,0 +1,44 @@
+#include "led.h"
+
+long * LED_GREEN = (long*)0xC0220130,
+     * LED_RED   = (long*)0xC0220134,
+     * LED_AF    = (long*)0xC0223030;
+
+void busy_wait(ulong time) {
+  BUSY_WAIT(time);
+}
+#undef  BUSY_WAIT
+#define BUSY_WAIT busy_wait
+
+void led_on(long * led) {
+  LED_ON(led);
+}
+
+void led_off(long * led) {
+  LED_OFF(led);
+}
+
+void led_flashx(long * led, ulong times, ulong delay) {
+  LED_FLASHX(led, times, delay);
+}
+
+void led_binx(long val, ulong delay) {
+  long i = 0;
+  led_flashx(val & 0x80000000 ? LED_RED : LED_GREEN, 1, delay);
+  for (i = 1; i < 32; ++i) {
+    if (i%4 == 0) busy_wait(delay);
+    if (i%8 == 0) busy_wait(delay);
+    if (i%16 == 0) busy_wait(delay);
+    val <<= 1;
+    led_flashx(val & 0x80000000 ? LED_RED : LED_GREEN, 1, delay);
+  }
+  led_flashx(LED_AF, 1, 2*delay);
+}
+
+void led_dumpx(long * mem, ulong len, ulong delay) {
+  long i = 0;
+  led_flashx(LED_AF, 1, 2*delay);
+  for (i = 0; i < len; ++i) {
+    led_binx(mem[i], delay);
+  }
+}
---
+++ platform/ixus130_sd1400/sub/100c/led.h
@@ -0,0 +1,55 @@
+#ifndef INCLUDED_LED_H
+#define INCLUDED_LED_H
+
+extern long * LED_GREEN, * LED_RED, * LED_AF;
+
+#ifndef DEFAULT_DELAY
+#define DEFAULT_DELAY 5000000
+#endif
+
+typedef unsigned long ulong;
+
+void busy_wait(ulong n);
+void led_on(long * led);
+void led_off(long * led);
+void led_flashx(long * led, ulong times, ulong delay);
+void led_binx(long val, ulong delay);
+void led_dumpx(long * mem, ulong len, ulong delay);
+
+/*
+static inline void led_flash(long * led, ulong times) {
+  led_flashx(led, times, DEFAULT_DELAY);
+}
+
+static inline void led_bin(long val) {
+  led_binx(val, DEFAULT_DELAY);
+}
+
+static inline void led_dump(long * mem, ulong len) {
+  led_dumpx(mem, len, DEFAULT_DELAY);
+}
+*/
+#define led_flash(led, times) led_flashx(led, times, DEFAULT_DELAY)
+#define led_bin(val) led_binx(val, DEFAULT_DELAY)
+#define led_dump(mem, len) led_dumpx(mem, len, DEFAULT_DELAY)
+
+#define BUSY_WAIT(delay) do { unsigned long i = (delay); while (--i) asm volatile("nop\n"); } while (0)
+
+#define LED_ON(led) do { *(long*)(led) = 0x46; } while(0)
+
+#define LED_OFF(led) do { *(long*)(led) = 0x44; } while(0)
+
+#define LED_FLASHX(led, times, delay) \
+  do { \
+    long i = (times);     \
+    while (i--) {         \
+      LED_ON((led));      \
+      BUSY_WAIT((delay)); \
+      LED_OFF((led));     \
+      BUSY_WAIT((delay)); \
+    }                     \
+  } while (0)
+
+#define LED_FLASH(led, times) LED_FLASHX((led), (times), DEFAULT_DELAY)
+
+#endif // INCLUDED_LED_H
