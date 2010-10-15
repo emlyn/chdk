97841ea52aca32bfc9443c67315198c09683524c
Emlyn <emlyn@corrin.name>
2010-10-15 16:33:34 +0200

    Cleanup resetcode/main

--- loader/ixus130_sd1400/resetcode/main.c
+++ loader/ixus130_sd1400/resetcode/main.c
@@ -6,99 +6,74 @@
  * Make sure stack is not used.
  */
 
-#define LED 0xC0220134 // IO, red
-#define DELAY 5000000
-
 void __attribute__((noreturn)) copy_and_restart(void *dst_void, const void *src_void, long length) {
 
-        //volatile unsigned *p = (void*)LED;       
-        //int counter;
+  char *dst = dst_void;
+  const char *src = src_void;
 
-        // DEBUG: blink led
-		
-        //counter = DELAY; *p = 0x46;  while (counter--) { asm("nop\n nop\n"); };
-        //counter = DELAY; *p = 0x44;  while (counter--) { asm("nop\n nop\n"); };
-		
+  if (src < dst && dst < src + length) {
+    /* Have to copy backwards */
+    src += length;
+    dst += length;
+    while (length--) {
+      *--dst = *--src;
+    }
+  } else {
+    while (length--) {
+      *dst++ = *src++;
+    }
+  }
 
-        {
-                char *dst = dst_void;
-                const char *src = src_void;
+  // DEBUG: jump to regular firmware-boot (causing a boot loop)
+  //dst_void = (void*) 0xFF810000;
 
-                if (src < dst && dst < src + length)
-                {
-                        /* Have to copy backwards */
-                        src += length;
-                        dst += length;
-                        while (length--)
-                        {
-                                *--dst = *--src;
-                        }
-                }
-                else
-                {
-                        while (length--)
-                        {
-                                *dst++ = *src++;
-                        }
-                }
-        }
+  // found at address 0xff83b9c0
+  asm volatile (
+        "mrs    r0, CPSR\n"
+        "bic    r0, r0, #63\n"  // 0x3f
+        "orr    r0, r0, #211\n" // 0xd3
+        "msr    CPSR_, r0\n"
+        "ldr    r1, =0xc0200000\n" // was: "[pc, #164]  ; ff83ba7c" 
+        "mvn    r0, #0\n"       // 0x0
+        "str    r0, [r1, #268]\n"
+        "str    r0, [r1, #12]\n"
+        "str    r0, [r1, #28]\n"
+        "str    r0, [r1, #44]\n"
+        "str    r0, [r1, #60]\n"
+        "str    r0, [r1, #76]\n"
+        "str    r0, [r1, #92]\n"
+        "str    r0, [r1, #108]\n"
+        "str    r0, [r1, #124]\n"
+        "str    r0, [r1, #140]\n"
+        "str    r0, [r1, #156]\n"
+        "str    r0, [r1, #172]\n"
+        "str    r0, [r1, #188]\n"
+        "str    r0, [r1, #204]\n"
+        "str    r0, [r1, #220]\n"
+        "str    r0, [r1, #236]\n"
+        "cmp    r4, #7\n"       // 0x7
+        "str    r0, [r1, #252]\n"
+        "popeq  {r4, pc}\n"
+        "ldr    r0, =0x78\n" // was: "mov ..., #120"
+        "mcr    15, 0, r0, cr1, cr0, 0\n"
+        "ldr    r0, =0x0\n" // was: "mov ..., #0"
+        "mcr    15, 0, r0, cr7, cr10, 4\n"
+        "mcr    15, 0, r0, cr7, cr5, 0\n"
+        "mcr    15, 0, r0, cr7, cr6, 0\n"
+        "ldr    r0, =0x80000006\n" // was: "mov ..., #-2147483642"
+        "mcr    15, 0, r0, cr9, cr1, 0\n"
+        "mcr    15, 0, r0, cr9, cr1, 1\n"
+        "mrc    15, 0, r0, cr1, cr0, 0\n"
+        "orr    r0, r0, #327680\n"      // 0x50000
+        "mcr    15, 0, r0, cr1, cr0, 0\n"
+        "ldr    r0, =0x12345678\n" // was: "[pc, #36]   ; ff83ba80" 
+        "ldr    r1, =0x80000000\n" // was: "mov ..., #-2147483648"
+        "str    r0, [r1, #4092]\n"
+        //"ldr    r0, =0xff810000\n" // changed to:
+        "mov    r0, %0\n"
+        "pop    {r4, lr}\n"
+        "bx     r0\n"
+        : : "r"(dst_void) : "memory","r0","r1","r2","r3","r4");
 
-        //DEBUG: blink again
-		
-        //counter = DELAY; *p = 0x46;  while (counter--) { asm("nop\n nop\n"); };
-        //counter = DELAY; *p = 0x44;  while (counter--) { asm("nop\n nop\n"); };
-		
-
-        // DEBUG: jump to regular firmware-boot (causing a boot loop)
-        //dst_void = (void*) 0xFF810000;
-
-        // found at address 0xff83b9c0
-        asm volatile (
-		"MRS     R0, CPSR\n"
-		"BIC     R0, R0, #0x3F\n"
-		"ORR     R0, R0, #0xD3\n"
-		"MSR     CPSR, R0\n"
-		"LDR     R1, =0xC0200000\n"
-		"MOV     R0, #0xFFFFFFFF\n"
-		"STR     R0, [R1,#0x10C]\n"
-		"STR     R0, [R1,#0xC]\n"
-		"STR     R0, [R1,#0x1C]\n"
-		"STR     R0, [R1,#0x2C]\n"
-		"STR     R0, [R1,#0x3C]\n"
-		"STR     R0, [R1,#0x4C]\n"
-		"STR     R0, [R1,#0x5C]\n"
-		"STR     R0, [R1,#0x6C]\n"
-		"STR     R0, [R1,#0x7C]\n"
-		"STR     R0, [R1,#0x8C]\n"
-		"STR     R0, [R1,#0x9C]\n"
-		"STR     R0, [R1,#0xAC]\n"
-		"STR     R0, [R1,#0xBC]\n"
-		"STR     R0, [R1,#0xCC]\n"
-		"STR     R0, [R1,#0xDC]\n"
-		"STR     R0, [R1,#0xEC]\n"
-		"CMP     R4, #7\n"
-		"STR     R0, [R1,#0xFC]\n"
-		"LDMEQFD SP!, {R4,PC}\n"
-		"MOV     R0, #0x78\n"
-		"MCR     p15, 0, R0,c1,c0\n"
-		"MOV     R0, #0\n"
-		"MCR     p15, 0, R0,c7,c10, 4\n"
-		"MCR     p15, 0, R0,c7,c5\n"
-		"MCR     p15, 0, R0,c7,c6\n"
-		"MOV     R0, #0x80000006\n"
-		"MCR     p15, 0, R0,c9,c1\n"
-		"MCR     p15, 0, R0,c9,c1, 1\n"
-		"MRC     p15, 0, R0,c1,c0\n"
-		"ORR     R0, R0, #0x50000\n"
-		"MCR     p15, 0, R0,c1,c0\n"
-		"LDR     R0, =0x12345678\n"
-		"MOV     R1, #0x80000000\n"
-		"STR     R0, [R1,#0xFFC]\n"
-//		"LDR     R0, =loc_FF810000\n" // changed to:
-		"mov     R0, %0\n"
-		"LDMFD   SP!, {R4,LR}\n"
-		"BX      R0\n"
-		: : "r"(dst_void) : "memory","r0","r1","r2","r3","r4");
-
-        while(1);
+  while(1);
 }
