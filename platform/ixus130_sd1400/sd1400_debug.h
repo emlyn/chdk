0655b180bfd91c54a75bcf7ff4a06de1b8f3bfa8
Emlyn <emlyn@corrin.name>
2010-10-25 17:45:20 +0200

    Use red led as debug

--- platform/ixus130_sd1400/sd1400_debug.h
+++ platform/ixus130_sd1400/sd1400_debug.h
@@ -1,6 +1,6 @@
-#define LED_IO_R 0xC0220134
 #define LED_IO_G 0xC0220130
-#define LED_IO_Y 0xC0220130
+#define LED_IO_R 0xC0220134
+#define LED_IO_Y 0xC0220134
 #define LED_AF   0xC0223030
 
 #define DELAY_1S     { int i; for (i = 0; i < 0x200000; i++) { asm volatile ("nop\n" ); } }
