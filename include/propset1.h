5e96389714cc778ee6cffc7c9c77bcf828a45e41
pixeldoc2000 <pixeldoc2000@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2010-11-15 19:44:00 +0000

    propset*.h cleanup
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@975 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- include/propset1.h
+++ include/propset1.h
@@ -5,43 +5,43 @@ constants for propset 1, most digic2 camera
 WARNING:
  The build uses tools/gen_propset_lua.sed to generate propset1.lua from this file
 */
-#define PROPCASE_DRIVE_MODE    				6
-#define PROPCASE_FOCUS_MODE    				12
-#define PROPCASE_FLASH_MODE       				16
-#define PROPCASE_FLASH_FIRE				79
+#define PROPCASE_DRIVE_MODE                      6
+#define PROPCASE_FOCUS_MODE                      12
+#define PROPCASE_FLASH_MODE                      16
+#define PROPCASE_FLASH_FIRE                      79
 // need corresponding values for propset2!!!
-#define PROPCASE_FLASH_ADJUST_MODE       	15
-// #define PROPCASE_FLASH_CORRECTION               28      
-#define PROPCASE_FLASH_MANUAL_OUTPUT    29
-//  #define PROPCASE_FOCUS_OK           67
-#define PROPCASE_USER_TV       				40
-#define PROPCASE_TV	        				69
-#define PROPCASE_USER_AV       				39
-#define PROPCASE_AV            				68
-#define PROPCASE_MIN_AV        				77
-#define PROPCASE_SV            				73
-#define PROPCASE_DELTA_SV      				70
-#define PROPCASE_SV_MARKET     				72
-#define PROPCASE_BV            				71
-#define PROPCASE_ISO_MODE      				21
-#define PROPCASE_SUBJECT_DIST1 				65
-#define PROPCASE_SUBJECT_DIST2 				66
-#define PROPCASE_SHOOTING     					205
-#define PROPCASE_IS_FLASH_READY   				221
-#define PROPCASE_OVEREXPOSURE 					76
-#define PROPCASE_SHOOTING_MODE					0
-#define PROPCASE_IS_MODE				        229
-#define PROPCASE_QUALITY				        23
-#define PROPCASE_RESOLUTION				        24
-#define PROPCASE_EV_CORRECTION_1				25
-#define PROPCASE_EV_CORRECTION_2				26
-#define PROPCASE_ORIENTATION_SENSOR			37
-#define PROPCASE_DIGITAL_ZOOM_STATE                            58
-#define PROPCASE_DIGITAL_ZOOM_POSITION                         57
-#define PROPCASE_DISPLAY_MODE                         181
-#define PROPCASE_BRACKET_MODE                         36 //missing for dryos!
-#define PROPCASE_FLASH_SYNC_CURTAIN			20
-#define PROPCASE_METERING_MODE				9
-#define PROPCASE_WB_ADJ                               100
-#define PROPCASE_OPTICAL_ZOOM_POSITION        99 // tested a540, sd400
+#define PROPCASE_FLASH_ADJUST_MODE               15
+//#define PROPCASE_FLASH_CORRECTION              28
+#define PROPCASE_FLASH_MANUAL_OUTPUT             29
+//#define PROPCASE_FOCUS_OK                      67
+#define PROPCASE_USER_TV                         40
+#define PROPCASE_TV	                             69
+#define PROPCASE_USER_AV                         39
+#define PROPCASE_AV                              68
+#define PROPCASE_MIN_AV                          77
+#define PROPCASE_SV                              73
+#define PROPCASE_DELTA_SV                        70
+#define PROPCASE_SV_MARKET                       72
+#define PROPCASE_BV                              71
+#define PROPCASE_ISO_MODE                        21
+#define PROPCASE_SUBJECT_DIST1                   65
+#define PROPCASE_SUBJECT_DIST2                   66
+#define PROPCASE_SHOOTING                        205
+#define PROPCASE_IS_FLASH_READY                  221
+#define PROPCASE_OVEREXPOSURE                    76
+#define PROPCASE_SHOOTING_MODE                   0
+#define PROPCASE_IS_MODE                         229
+#define PROPCASE_QUALITY                         23
+#define PROPCASE_RESOLUTION                      24
+#define PROPCASE_EV_CORRECTION_1                 25
+#define PROPCASE_EV_CORRECTION_2                 26
+#define PROPCASE_ORIENTATION_SENSOR              37
+#define PROPCASE_DIGITAL_ZOOM_STATE              58
+#define PROPCASE_DIGITAL_ZOOM_POSITION           57
+#define PROPCASE_DISPLAY_MODE                    181
+#define PROPCASE_BRACKET_MODE                    36 //missing for dryos!
+#define PROPCASE_FLASH_SYNC_CURTAIN              20
+#define PROPCASE_METERING_MODE                   9
+#define PROPCASE_WB_ADJ                          100
+#define PROPCASE_OPTICAL_ZOOM_POSITION           99 // tested a540, sd400
 #endif
