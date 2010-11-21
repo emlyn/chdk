a4cd8b488ed56ac67ca9e746758ed3bc93c70296
Emlyn <emlyn@corrin.name>
2010-11-21 15:37:20 +0100

    Aspect ratio correction

--- include/camera.h
+++ include/camera.h
@@ -2056,27 +2056,49 @@
     //0.459395 -0.129083 -0.068085
     //-0.055837 0.400827 0.020378
     //-0.004622 0.048662 0.139970
-/*
-    #undef CAM_USES_ASPECT_CORRECTION
-    #undef CAM_USES_ASPECT_YCORRECTION
-    #define CAM_USES_ASPECT_CORRECTION  1  //camera uses the modified graphics primitives to map screens an viewports to buffers more sized
+
+    //nandoide sept-2009
+    #undef CAM_USES_ASPECT_CORRECTION   
+    #undef CAM_USES_ASPECT_YCORRECTION  
+    #define CAM_USES_ASPECT_CORRECTION  1  //camera uses the modified graphics primitives to map screens an viewports to buffers more sized 
     #define CAM_USES_ASPECT_YCORRECTION  0  //only uses mappings on x coordinate
-    #undef GAMES_SCREEN_WIDTH
-    #undef GAMES_SCREEN_HEIGHT
-    #define GAMES_SCREEN_WIDTH 360
-    #define GAMES_SCREEN_HEIGHT 240
-    #undef ASPECT_GAMES_XCORRECTION
-    // 720/360=2 same aspect than grids and viewport but another approach: there is a lot of corrections to do in game's code, and we decide to paint directly on display buffer wirh another resolution
-    // used by gui.c that configures the draw environment (trhough new draw_gui function) depending on gui_mode: we have then 360x240 for games (but deformed output:circles are not circles) and 320x240 for
-    // other modes in perfect aspect ratio 4/3: slightly better visualization: file menus more readable, ...
-    #define ASPECT_GAMES_XCORRECTION(x)   ( ((x)<<1) )  
-    #undef ASPECT_GAMES_YCORRECTION
-    #define ASPECT_GAMES_YCORRECTION(y)   ( (y) )  //none
+
+// reyalp TODO not sure this stuff belongs in camera.h there will probably only be a few different setups
+// maybe we can just have one CAM_ options that picks what to use ?
+// values need to be better documented
+	// reyalp - I guess these are bitmap ?
+    //default mappings
+    #undef ASPECT_XCORRECTION
+//#define ASPECT_XCORRECTION(x)  ( ( ((x)<<3) + (x) )  >>2 )   //correction x*screen_buffer_width/screen_width = x*720/320 = x*9/4 = (x<<3 + x)>>2
+    #define ASPECT_XCORRECTION(x)  ( (x) + ((x) >> 1) )
+    
+    //grids
     #undef ASPECT_GRID_XCORRECTION
     #define ASPECT_GRID_XCORRECTION(x)  ( ((x)<<3)/9  )  //grids are designed on a 360x240 basis and screen is 320x240, we need x*320/360=x*8/9
     #undef ASPECT_GRID_YCORRECTION
     #define ASPECT_GRID_YCORRECTION(y)  ( (y) )       //y correction for grids  made on a 360x240 As the buffer is 720x240 we have no correction here.
-*/
+    
+    //viewport
+    #undef ASPECT_VIEWPORT_XCORRECTION 
+    #define ASPECT_VIEWPORT_XCORRECTION(x) ASPECT_GRID_XCORRECTION(x) //viewport is 360x240 and screen 320x240, we need x*320/360=x*8/9, equal than grids, used by edgeoverlay
+    #undef ASPECT_VIEWPORT_YCORRECTION 
+    #define ASPECT_VIEWPORT_YCORRECTION(y) ( (y) ) 
+    #undef EDGE_HMARGIN 
+    #define EDGE_HMARGIN 28
+    
+    //games mappings
+   #undef GAMES_SCREEN_WIDTH
+   #undef GAMES_SCREEN_HEIGHT
+   #define GAMES_SCREEN_WIDTH 360
+   #define GAMES_SCREEN_HEIGHT 240
+   #undef ASPECT_GAMES_XCORRECTION
+   // 720/360=2 same aspect than grids and viewport but another approach: there is a lot of corrections to do in game's code, and we decide to paint directly on display buffer wirh another resolution
+   // used by gui.c that configures the draw environment (through new draw_gui function) depending on gui_mode: we have then 360x240 for games (but deformed output:circles are not circles) and 320x240 for
+   // other modes in perfect aspect ratio 4/3: slightly better visualization: file menus more readable, ...
+   #define ASPECT_GAMES_XCORRECTION(x)   ( ((x)<<1) )  
+   #undef ASPECT_GAMES_YCORRECTION
+   #define ASPECT_GAMES_YCORRECTION(y)   ( (y) )  //none
+
     #define cam_CalibrationIlluminant1  17			// Standard Light A
 
 	// cropping
