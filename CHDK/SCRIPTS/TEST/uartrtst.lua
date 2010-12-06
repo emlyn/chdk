0aec886073a4311bc5532d1aa3d699fc5a4d4c8c
pixeldoc2000 <pixeldoc2000@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2010-12-06 22:31:00 +0000

    uartrtst.lua updated (added native function call support check and delay)
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@1001 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- CHDK/SCRIPTS/TEST/uartrtst.lua
+++ CHDK/SCRIPTS/TEST/uartrtst.lua
@@ -3,6 +3,10 @@
 requires CHDK built with native function call support
 --]]
 
+if (type(call_event_proc) ~= "function" ) then
+    error("ERROR: requires CHDK build with native function call support.")
+end
+
 -- include lualib module (uartr.lua)
 uartr=require("uartr")
 
@@ -12,7 +16,13 @@ uartr.start("A/UARTRTST.LOG", 1)
 -- redirect UART to random filesname like "00014540.log"
 --uartr.start()
 
+-- Short delay to avoid trouble, ToDo: really necessary ?
+sleep(100)
+
 -- os.date() return something like "Sat Dec  4 01:54:01 2010"
 call_event_proc("Printf", os.date().." UART redirect Test (uartrtst.lua) OK\n");
+
+-- Short delay to avoid trouble, ToDo: really necessary ?
+sleep(100)
 
 uartr.stop()
