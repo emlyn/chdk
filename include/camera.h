28ce4b62f2182f4711aa62d55dddb6d2cae02637
fe50 <fe50@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2010-12-28 12:50:22 +0000

    * added DNG support for G9, by CHDKLover, http://my-trac.assembla.com/chdkde/changeset/516/ , http://forum.chdk-treff.de/viewtopic.php?f=11&t=2198
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@1006 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- include/camera.h
+++ include/camera.h
@@ -137,21 +137,43 @@
 //----------------------------------------------------------
 
 #elif defined (CAMERA_g9)
-    #define CAM_PROPSET                 2
-    #define CAM_DRYOS                   1
+	#define CAM_PROPSET							2
+	#define CAM_DRYOS							1
 
-    #define CAM_RAW_ROWPIX              4104   // for 12 MP
-    #define CAM_RAW_ROWS                3048   // for 12 MP
-    #define CAM_HAS_ND_FILTER           1
-    #define CAM_ADJUSTABLE_ALT_BUTTON   1
-    #undef  CAM_EMUL_KEYPRESS_DURATION
-    #define CAM_EMUL_KEYPRESS_DURATION  10
-    #define CAM_AF_SCAN_DURING_VIDEO_RECORD 1
-    #define CAM_HAS_JOGDIAL             1
-//  #define CAM_CONSOLE_LOG_ENABLED     1
-    #define CAM_BRACKETING              1
-    #define CAM_MULTIPART               1
-    #define CAM_EXT_TV_RANGE            1
+	#define CAM_RAW_ROWPIX						4104	// for 12 MP
+	#define CAM_RAW_ROWS						3048	// for 12 MP
+	#define CAM_HAS_ND_FILTER					1
+	#define CAM_ADJUSTABLE_ALT_BUTTON			1
+	#undef  CAM_EMUL_KEYPRESS_DURATION
+	#define CAM_EMUL_KEYPRESS_DURATION			10
+	#define CAM_AF_SCAN_DURING_VIDEO_RECORD		1
+	#define CAM_HAS_JOGDIAL						1
+	// #define CAM_CONSOLE_LOG_ENABLED			1
+	#define CAM_BRACKETING						1
+	#define CAM_MULTIPART						1
+	#define CAM_EXT_TV_RANGE					1
+
+	// camera name
+	#define PARAM_CAMERA_NAME					4	// parameter number for GetParameterData
+
+	#define DNG_SUPPORT							1
+	// pattern
+	#define cam_CFAPattern						0x02010100	// Red  Green  Green  Blue
+	// color (alternativ: http://lclevy.free.fr/cr2/)
+	#define CAM_COLORMATRIX1 \
+	1679727, 1000000,  422803, 1000000,  803343, 1000000, \
+	 220204, 1000000, 1784845, 1000000,  -63290, 1000000, \
+	-282882, 1000000, -475034, 1000000, 4502592, 1000000
+
+	#define cam_CalibrationIlluminant1			1	// Daylight
+
+	// cropping
+	#define CAM_JPEG_WIDTH						4000
+	#define CAM_JPEG_HEIGHT						3000
+	#define CAM_ACTIVE_AREA_X1					52
+	#define CAM_ACTIVE_AREA_Y1					14
+	#define CAM_ACTIVE_AREA_X2					4076
+	#define CAM_ACTIVE_AREA_Y2					3030
 //----------------------------------------------------------
 
 
