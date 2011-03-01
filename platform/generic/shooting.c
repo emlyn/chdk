25fc92755ef17957a287262b46e0ef3be1b0ad4c
pixeldoc2000 <pixeldoc2000@6794e30b-3f2a-0410-a806-a2bbca1c07ff>
2011-03-01 22:24:01 +0000

    shooting_get_iso_str() moved to platform/generic/shooting.c and disabled (unused)
    git-svn-id: https://tools.assembla.com/svn/chdk/trunk@1075 6794e30b-3f2a-0410-a806-a2bbca1c07ff

--- platform/generic/shooting.c
+++ platform/generic/shooting.c
@@ -1446,3 +1446,16 @@ int /*__attribute__((weak))*/ mode_get(void) {
 
     return (mode);
 }
+
+/*
+char* shooting_get_iso_str() {
+    short int isov;
+    long i;
+    _GetPropertyCase(PROPCASE_ISO_MODE, &isov, sizeof(isov));
+    for(i=0; i<ISO_SIZE; i++) {
+        if(iso_table[i].prop_id == isov)
+            return (char*)iso_table[i].name;
+    }
+    return (char*) "?";
+}
+*/
